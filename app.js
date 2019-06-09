d01d65a6dcf3ca3bb043a17c20d813cace43fbe5
Tracy Dean Blackadder <tracey.blackadder@realestate.com.au>
2019-06-09 10:13:02 +1000

    minor changes

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// Side navigation
+function w3_open() {
+  var x = document.getElementById("mySidebar");
+  x.style.width = "100%";
+  x.style.fontSize = "40px";
+  x.style.paddingTop = "10%";
+  x.style.display = "block";
+}
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+// Accordions
+function myAccFunc(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
